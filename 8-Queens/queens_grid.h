e5145a9afee9d51a502161d686a03173e5c21245
Arjun Dilip Kumar <arjundilipkumar93@gmail.com>
2014-08-19 16:32:19 -0500

    Need to implement new method for queen placement

--- 8-Queens/queens_grid.h
+++ 8-Queens/queens_grid.h
@@ -26,7 +26,7 @@ class queens_grid {
 		// Sets conflict for all squares
 		void removeQueen(int row, int col);
 		// Removes queen from row, col and adjusts conflict
-		int returnNextSlot();
+		int returnNextSlot(int currentQueen);
 		// Returns the next available slot for queen
 		// If returns -1 then no available slot
 		void setConflicts(std::vector<int> queenPositions);
@@ -103,7 +103,24 @@ void queens_grid::removeQueen(int row, int col) {
 	setConflict(false, row, col);
 }
 
-int queens_grid::returnNextSlot() {
+int queens_grid::returnNextSlot(int currentQueen) {
+
+
+	// Include grid conflict and queenPlaced check below
+	int row = currentQueen/8, col = currentQueen%8;
+	if ((col + 2 <= 7) && (row + 1 <= 7) && grid[row+1][col+2].conflict == false && grid[row+1][col+2].queenPlaced == false) {
+		return (row + col +3);
+	}
+	if ((col - 2 >= 0) && (row + 1 <= 7) && grid[row+1][col-2].conflict == false && grid[row+1][col-2].queenPlaced == false) {
+		return (row + col - 1);
+	}
+	if ((col -1 >= 0) && (row + 2 <= 7) && grid[row+2][col-1].conflict == false && grid[row+2][col-1].queenPlaced == false) {
+		return (row + col + 1);
+	}
+	if ((col +1 <= 7) && (row + 2 <= 7) && grid[row+2][col+1].conflict == false && grid[row+2][col+1].queenPlaced == false) {
+		return (row + col + 3);
+	}
+	// Brute Force Placement Below
 	for (int i=0; i < 8; i++) {
 		for (int j=0; j < 8; j++) {
 			if (grid[i][j].queenPlaced == false && grid[i][j].conflict == false) {
@@ -123,8 +140,25 @@ void queens_grid::setConflicts(std::vector<int> queenPositions) {
 void queens_grid::completeGrid() {
 	std::vector<int> queenPositions;
 	int priorQueen = -1;
+	/* TESTING
+	queenPositions.push_back(5);
+	setQueen(0, 5);
+	queenPositions.push_back(11);
+	setQueen(1, 3);
+	queenPositions.push_back(22);
+	setQueen(2, 6);
+	queenPositions.push_back(24);
+	setQueen(3, 0);
+	queenPositions.push_back(39);
+	setQueen(4, 7);
+	queenPositions.push_back(41);
+	setQueen(5, 1);
+	queenPositions.push_back(52);
+	setQueen(6, 4);
+	*/
+	queenPositions.push_back(5);
 	while (queenCount != 8) {
-		int temp = returnNextSlot();
+		int temp = returnNextSlot(queenPositions.back());
 		// Checking if impossible to place next queen -> backtrack
 		if (temp == -1) {
 			if (priorQueen != -1) {
